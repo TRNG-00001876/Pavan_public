2dffef83a94e67a75c056d1bbd264fba87aaa413
Rajpavankotla <kotlapavankumar26688@gmail.com>
2024-04-12 18:37:55 +0530

    ArrayList Added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
+import  java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        ArrayList<String> players = new ArrayList<>();
+
+        players.add(0, "Bryant");
+        players.add("Wade");
+        players.add("Pavan");
+        players.add("Reddy");
+        players.add("Kohli");
+        players.add("Kumar");
+        players.set(0,"Raja");
+        players.remove(1);
+        System.out.println(players.size());
+        ArrayList <String> slicedArray=new ArrayList<>(players.subList(2,players.size()));
+        System.out.println(slicedArray);
+        System.out.println(players);
     }
 }
